2d56ef847eec91d96e5ad4cbc0be6dba7db96993
pkanev <pvkanev@gmail.com>
2016-05-22 17:29:51 +0300

    Added solutions to Distance between Vertices and Areas in Matrix

---
+++ Algorithms/Areas-In-Matrix/AreasInMatrix.cs
@@ -0,0 +1,98 @@
+﻿namespace Areas_In_Matrix
+{
+    using System;
+    using System.Collections.Generic;
+
+    class AreasInMatrix
+    {
+        private static char[,] matrix;
+        private static bool[,] visited;
+
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine().Substring(16));
+            string[] lines = new string[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                lines[i] = Console.ReadLine();
+            }
+
+            //int rows = int.Parse("Number of rows: 6".Substring(16));
+            //var lines = new string[]
+            //{
+            //    "aacccaac",
+            //    "baaaaccc", 
+            //    "baabaccc",
+            //    "bbdaaccc",
+            //    "ccdccccc",
+            //    "ccdccccc"
+            //};
+
+            int cols = lines[0].Length;
+            matrix = new char[rows, cols];
+            visited = new bool[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = lines[row][col];
+                }
+            }
+
+            var mappedAreas = new SortedDictionary<char, int>();
+            int areas = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (!visited[row, col])
+                    {
+                        char label = matrix[row, col];
+                        if (!mappedAreas.ContainsKey(label))
+                        {
+                            mappedAreas[label] = 0;
+                        }
+
+                        mappedAreas[label] ++;
+
+                        MarkArea(label, row, col);
+                        areas++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Areas: {0}", areas);
+            foreach (var area in mappedAreas)
+            {
+                Console.WriteLine("Letter '{0}' -> {1}", area.Key, area.Value);
+            }
+
+        }
+
+        private static void MarkArea(char label, int row, int col)
+        {
+            if (row < 0 || row >= matrix.GetLength(0) || col < 0 || col >= matrix.GetLength(1))
+            {
+                return;
+            }
+
+            if (matrix[row, col] != label)
+            {
+                return;
+            }
+
+            if (visited[row, col])
+            {
+                return;
+            }
+
+            visited[row, col] = true;
+            MarkArea(label, row - 1, col);
+            MarkArea(label, row + 1, col);
+            MarkArea(label, row, col - 1);
+            MarkArea(label, row, col + 1);
+        }
+    }
+}
---
+++ Algorithms/Distance-Between-Vertices/DistanceBetweenVertices.cs
@@ -0,0 +1,98 @@
+﻿namespace Distance_Between_Vertices
+{
+    using System;
+    using System.Collections.Generic;
+
+    class DistanceBetweenVertices
+    {
+        private static List<int>[] graph;
+
+        static void Main()
+        {
+            graph = new List<int>[]
+            {
+                new List<int>() {0}, //0
+                new List<int> {4}, //1
+                new List<int> {4}, //2
+                new List<int> {4, 5}, //3
+                new List<int> {6}, //4
+                new List<int> {3, 7, 8}, //5
+                new List<int>(), //6
+                new List<int> {8}, //7
+                new List<int>(), //8
+            };
+            
+            var distancesToFind = new KeyValuePair<int, int>[]
+            {
+                new KeyValuePair<int, int>(1, 6),
+                new KeyValuePair<int, int>(1, 5),
+                new KeyValuePair<int, int>(5, 6),
+                new KeyValuePair<int, int>(5, 8),
+                new KeyValuePair<int, int>(0, 0),
+            };
+
+            foreach (var pair in distancesToFind)
+            {
+                var steps = CalculateSteps(pair.Key, pair.Value);
+                Console.WriteLine("{{{0}}}, {{{1}}} -> {{{2}}}", pair.Key, pair.Value, steps);
+            }
+        }
+
+        private static int CalculateSteps(int start, int target)
+        {
+            if (start == target)
+            {
+                return 0;
+            }
+
+            var queue = new Queue<int>();
+            var visited = new bool[graph.Length];
+            var previousNodes = new Dictionary<int, int>();
+            bool isReached = false;
+
+            queue.Enqueue(start);
+            visited[start] = true;
+            
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                foreach (var childNode in graph[node])
+                {
+                    if (!visited[childNode])
+                    {
+                        previousNodes[childNode] = node;
+
+                        if (childNode == target)
+                        {
+                            isReached = true;
+                            break;
+                        }
+
+                        queue.Enqueue(childNode);
+                        visited[childNode] = true;
+                    }
+                }
+            }
+
+            if (isReached)
+            {
+                return CalculateSteps(start, target, previousNodes);
+            }
+            
+            return -1;
+        }
+
+        private static int CalculateSteps(int start, int target, Dictionary<int, int> previousNodes)
+        {
+            int steps = 0;
+            while (target != start)
+            {
+                target = previousNodes[target];
+                steps++;
+            }
+
+            return steps;
+        }
+    }
+}
